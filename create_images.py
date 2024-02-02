5152239c529b7fb0970af24da4198103278d831b
OMBowkerBBC <oliver.bowker@bbc.co.uk>
2024-02-02 11:59:38 +0000

    background start

--- create_images.py
+++ create_images.py
@@ -1,8 +1,19 @@
-import os, itertools
+import itertools, os, sys
+
 if __name__ == "__main__":
-  puml_files = []
+  removeImages: bool = sys.argv[1] == '--remove-images'
+  puml_files: [str] = []
+  png_files: [str] = []
+
   for (root, _ ,files) in os.walk("./diagrams"):
     puml_files.append([f"{root}/{file}" for file in files if file.endswith(".puml")])
+    png_files.append([f"{root}/{file}" for file in files if file.endswith(".png")])
+
+  if removeImages:
+    print("Removing all images...")
+    for file in list(itertools.chain(*png_files)):
+      os.remove(file)
+  
 
   # Use unpacking to flatten list of lists
   for file in list(itertools.chain(*puml_files)):
